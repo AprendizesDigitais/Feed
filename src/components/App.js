a2d8d764ce69de744ebe6b988d31eff543a94e4d
GabrielSegatto <gabriel.segatto58@gmail.com>
2018-11-28 13:01:51 -0200

    att codigo

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,12 @@ import Post from './Post';
 
 export default class App extends React.Component {
     render(){
-        return(2
+        return(
             <div>
                 <h1>Olá mundo</h1>
-                <Post title="Aprendendo React"/>
-                <Post title="Eu vou fazer o novo Facebook!"/>
-                <Post title="Ele vai ter varios textoes de tia"/>
+                <Post title="Aprendendo React" />
+                <Post title="Eu vou fazer o novo Facebook!" />
+                <Post title="Ele vai ter varios textoes de tia" />
             </div>
         )
     }
